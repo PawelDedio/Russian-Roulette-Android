5c79cb2ca4100b9716a1c46c97e928fc455bbb62
pawel.dedio <paweldedio@gmail.com>
2017-12-15 15:35:36 +0100

    Part of logic for roulette widget

--- app/src/main/java/com/dedio/russianroulette/views/roulette/RouletteViewModel.kt
+++ app/src/main/java/com/dedio/russianroulette/views/roulette/RouletteViewModel.kt
@@ -1,11 +1,17 @@
 package com.dedio.russianroulette.views.roulette
 
+import android.support.animation.DynamicAnimation
+import android.support.animation.FlingAnimation
+import android.support.animation.FloatValueHolder
+import android.util.Log
+import android.util.TimeUtils
 import android.view.MotionEvent
 import android.view.MotionEvent.*
 import android.view.View
 import android.widget.ImageView
 import java.lang.Math.abs
 import java.lang.Math.pow
+import java.util.concurrent.TimeUnit
 
 class RouletteViewModel {
 
@@ -13,12 +19,19 @@ class RouletteViewModel {
 
     private var downTouchX = 0f
     private var downTouchY = 0f
-    private var centerX: Float = 0f
-    private var centerY: Float = 0f
-    private val moveTolerance = 0.2
+    private var centerX = 0f
+    private var centerY = 0f
+    private val moveTolerance = 0.1
     private val locationInWindow: IntArray = IntArray(2)
     private var imageWidth = 0
     private var imageHeight = 0
+
+    private var oldMoveTime: Long = 0
+    private var oldMoveAngle = 0f
+    private var lastMoveTime: Long = 0
+    private var lastMoveAngle = 0f
+
+    private var throwAnim: FlingAnimation? = null
 
 
     fun initPosition(view: RouletteView, imageView: ImageView) {
@@ -43,17 +56,32 @@ class RouletteViewModel {
 
         when(event.action) {
             ACTION_DOWN -> {
+                throwAnim?.cancel()
                 downTouchX = event.rawX
                 downTouchY = event.rawY
+                log("event: down, x: ${event.rawX} y: ${event.rawY}")
             }
 
             ACTION_MOVE -> {
+                oldMoveAngle = state.angle.plus(0)
                 makeMove(event)
+                lastMoveAngle = state.angle.plus(0)
+
+                oldMoveTime = lastMoveTime
+                lastMoveTime = System.currentTimeMillis()
+                log("event: move, x: ${event.rawX} y: ${event.rawY} angle: ${state.angle}")
             }
 
             ACTION_UP -> {
+                log("event: up, x: ${event.rawX} y: ${event.rawY}")
+                if (System.currentTimeMillis() - lastMoveTime > 10) {
+                    log("event up: ############## without throw ##############")
+                } else {
+                    log("event up: ############## throw ##############")
+                    animateThrow()
                 }
             }
+        }
         return true
     }
 
@@ -133,5 +161,27 @@ class RouletteViewModel {
         ratio = (ratioX + ratioY) / 2
         degree = 90 * ratio + startDegree
         state.angle = degree
+    }
+
+    private fun animateThrow() {
+        val holder = FloatValueHolder(state.angle)
+        val distance = abs(lastMoveAngle - oldMoveAngle)
+        val time: Float = (lastMoveTime - oldMoveTime).toFloat() / TimeUnit.SECONDS.toMillis(1)
+        var velocity: Float = distance / time
+        if (lastMoveAngle < oldMoveAngle) {
+            velocity *= -1
+        }
+
+        throwAnim = FlingAnimation(holder)
+                .setStartVelocity(velocity)
+                .setFriction(0.18f)
+                .addUpdateListener { dynamicAnimation, value, velocity ->
+                    state.angle = value % 360
+                }
+        throwAnim?.start()
+    }
+
+    private fun log(text: String) {
+        Log.e("RouletteViewModel", text)
     }
 }
